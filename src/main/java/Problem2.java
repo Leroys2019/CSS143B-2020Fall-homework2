bf832f91baf0345b549895367048d2841d53a50b
Leroy Smith <leroys2019@gmail.com>
2020-10-17 23:38:58 -0700

    Test passes P2

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,12 +1,26 @@
 public class Problem2 {
 
     /*
-    Describe your algorithm here in up to 5 sentences:
+    My algorithm uses multiple sets of ints to change the count each time an integer changes within the array to show
+    the length of an array without any duplicates. Every time an integer doesn't match the number previously set,
+    the algorithm changes the set number and count how many times the set number has changed.
      */
 
     // Do not change signature (function name, parameters and return type)
     // Do not use extra space for another array. Solution to be in-pace with O(1) extra memory
     public static int remove(int[] nums) {
-        return Integer.MAX_VALUE; // place holder
+        int k = 1;
+        int i;
+        int main = nums[0];
+        for(i = 1; i < k; i++){
+            if(nums[i] != main){
+                main = nums[i];
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
-}
+         // place holder
+    }
+
